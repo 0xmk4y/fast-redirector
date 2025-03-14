f7d7baf7aea868c1bf5aa193d624265093f4126c
0xmk4y <bismarck.a360@gmail.com>
2025-03-14 15:29:06 +0000

    change original_link to original_url

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ python -m pip install --upgrade pip
 echo "*" > .venv/.gitignore
 pip install "fastapi[standard]"
 pip install supabase
+
+docker build -t fast-redirctor .
+docker run -d -p 80:80 fast-redirctor
--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 def supabase_client() -> Client:
     url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
@@ -18,20 +18,24 @@ def supabase_client() -> Client:
 @app.get("/{path}")
 def redirector(path: str):
     supabase: Client = supabase_client()
-    if len(path) >= 6:
-        raise HTTPException(status_code=400)
-    response = supabase.table("urls").select("*").eq("short", path).execute()
-    
-    if response.data:
-        original_link = response.data[0].get("original_link")
-        if original_link:
-            # Ensure the URL starts with "http://" or "https://"
-            if not original_link.startswith(("http://", "https://")):
-                original_link = "https://" + original_link  
-            print(original_link)
-            return RedirectResponse(url=original_link, status_code=302)
-    
-    raise HTTPException(status_code=404, detail="No matching record found")
+    try:
+        if len(path) >= 6:
+            raise HTTPException(status_code=400)
+        response = supabase.table("urls").select("*").eq("short", path).execute()
+        print(response)
+        
+        if response.data:
+            original_link = response.data[0].get("original_link")
+            if original_link:
+                # Ensure the URL starts with "http://" or "https://"
+                if not original_link.startswith(("http://", "https://")):
+                    original_link = "https://" + original_link  
+                print(original_link)
+                return RedirectResponse(url=original_link, status_code=302)
+        
+        raise HTTPException(status_code=404, detail="error")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="error")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
